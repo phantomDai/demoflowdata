fb28ae4f5aab49410fcfacc6ed572404bbbfd16b
daihe <daihepeng@sina.cn>
2018-07-31 10:39:51 +0800

    从 hbase中获取最新的数据

---
+++ src/main/java/util/ConfigurationHbase.java
@@ -0,0 +1,12 @@
+package util;
+
+public class ConfigurationHbase {
+
+    /**
+     * the name of table that save information of devices
+     */
+    public static final String TABLENAME = "file_table";
+
+
+
+}
---
+++ src/test/java/com/example/demoflowdata/DemoflowdataApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.demoflowdata;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoflowdataApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
